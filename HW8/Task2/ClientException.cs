9a68146e5055498924af90432a19580439eaabaf
DARYA.BARYSAVETS <darya.barysavets@intapp.com>
2022-12-05 15:40:57 +0200

    Homework 8

---
+++ HW8/Task2/ClientException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8.Task2
+{
+    public class ClientException : Exception
+    {
+        private string? _nation;
+        private int _age;
+
+        public ClientException(string nation): base($"Bad nation: {nation}")
+        {
+            this._nation = nation;
+        }
+
+        public ClientException(int age) : base($"You are very old: {age}")
+        {
+            this._age = age;
+        }
+    }
+}
---
+++ HW8/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8.Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            try
+            {
+                Client client = new Client() { Experience = 1, Name = "Masha" };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+            try
+            {
+                Client newClient = new Client() { Experience = 3, Name = "Pasha", Nation = "Russian" };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+            try
+            {
+                Client oldClient = new Client() { Experience = 50, Name = "Dima", Nation = "Belarus", Age = 75 };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
